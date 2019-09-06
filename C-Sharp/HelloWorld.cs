5981d70209669857508c206e1b91d8b0e5b541ff
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-09-06 12:28:19 +0530

    Create HelloWorld.cs

---
+++ C-Sharp/HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Nishkarsh
+{
+class HelloWorld
+{
+    static void Main() 
+    {
+        /* No need to specify cmd line argument for Main() */
+        Console.WriteLine("Hello World!");
+        Console.ReadKey();
+    }
+}
+}
