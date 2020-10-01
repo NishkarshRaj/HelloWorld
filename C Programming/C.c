0d836dc2d5fbe97384c1e884291184ea53d0ecb0
OSB1311 <69256144+OSB1311@users.noreply.github.com>
2020-10-01 23:30:30 +0530

    Update C.c

--- C Programming/C.c
+++ C Programming/C.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
-int main()
+#include<stdio.h>/*-------------Declaring c library-------------*/
+int main()/*---------------WE can use void main() also-----*/
 {
+	/*---------- Printing hello world----------*/
 	printf("Hello World!");
 	return 0;
 }
