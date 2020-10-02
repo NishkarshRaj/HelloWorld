1e1bd8bac10b4075214e7faa9d0a8287a9ad39c3
OSB1311 <69256144+OSB1311@users.noreply.github.com>
2020-10-02 07:31:56 +0530

    Update C.c (#66)
    * Update C.c
    
    * Update C.c
    
    Co-authored-by: Nishkarsh Raj <500060720@stu.upes.ac.in>

--- C Programming/C.c
+++ C Programming/C.c
@@ -1,6 +1,10 @@
-#include<stdio.h>
+// Add header files
+#include<stdio.h> 
+
+// Main Driver Code
 int main()
 {
+	// Using printf to send message to console out
 	printf("Hello World!");
 	return 0;
 }
