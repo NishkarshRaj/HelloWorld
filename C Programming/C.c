6d21af2a317e35bb19635997784566e33d096ee9
Mr.Zedd <72000565+Mr-zedd@users.noreply.github.com>
2020-10-06 12:23:04 +0530

    Update C.c (#69)
    I have modified it so that person reading this program for the first time can better understand it.

--- C Programming/C.c
+++ C Programming/C.c
@@ -1,7 +1,12 @@
-// Add header files
-#include<stdio.h> 
-
-// Main Driver Code
+/*stdio.h is a library that operates input output functions of the program */
+#include<stdio.h>
+/*The program is written in the main function,this main does not return a value*/
+void main()
+{
+	// Using printf to send message to console out
+	printf("Hello World!");
+}
+/*The program is written in the main function,this main does not return a value*/
 int main()
 {
 	// Using printf to send message to console out
